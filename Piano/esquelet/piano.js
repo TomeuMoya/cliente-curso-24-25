049940ffda30d0df0050f6475fe40a04c8db5b1a
TomeuMoya <bartomeumoya@paucasesnoves.cat>
2024-10-08 16:26:56 +0200

    Codi js acbat, estructura canviada i funcional

--- Piano/esquelet/piano.js
+++ Piano/esquelet/piano.js
@@ -9,11 +9,30 @@ let cerca = [];
 
 let partitures = [
 
-    {nom: "La Balanguera", notes: [new Nota('Do', false), new Nota('Re', false), new Nota('Mi', false), new Nota('Fa', false), 
-    new Nota('Fa', false), new Nota('Sol', false), new Nota('Sol', false), new Nota('La', true), new Nota('La', true)]},
+    {nom: "La Balanguera", 
+    notes: [new Nota('Do', false), 
+            new Nota('Re', false), 
+            new Nota('Mi', false), 
+            new Nota('Fa', false), 
+            new Nota('Fa', false), 
+            new Nota('Sol', false), 
+            new Nota('Sol', false), 
+            new Nota('La', true), 
+            new Nota('La', true)]},
 
-    {nom: "Happy Birthday", notes: [new Nota('Do', false), new Nota('Do', false), new Nota('Re', false), new Nota('Do', false), new Nota('Fa', false),
-    new Nota('Mi', false), new Nota('Do', false), new Nota('Do', false), new Nota('Re', false), new Nota('Do', false), new Nota('Sol', false), new Nota('Fa', false)]}
+    {nom: "Happy Birthday", 
+    notes: [new Nota('Do', false), 
+            new Nota('Do', false), 
+            new Nota('Re', false), 
+            new Nota('Do', false), 
+            new Nota('Fa', false),
+            new Nota('Mi', false), 
+            new Nota('Do', false), 
+            new Nota('Do', false), 
+            new Nota('Re', false), 
+            new Nota('Do', false), 
+            new Nota('Sol', false), 
+            new Nota('Fa', false)]}
 
 ];
 
@@ -23,29 +42,51 @@ function addCerca(nom, sostinguda) {
 
 }
 
-function cercador(partitures, cerca) {
+function compararNotas(partitures, cerca) {
 
-    let resultado = [];
+    for(let i = 0; i <= partitures.length - cerca.length; i++) {
 
-    for(let i = 0; i < partitures.length, i++;) {
+        let trobat = true;
 
-        for(let j = 0; i < cerca.length; i++) {
+        for(let j = 0; j < cerca.length; j++) {
 
-            if (partitures.notes == cerca) {
+            if (partitures[i + j].nom !== cerca[j].nom || partitures[i + j].sostinguda !== cerca[j].sostinguda) {
 
-                resultado.push(partitures.nom)
+                trobat = false;
                 break;
+                
+            } 
 
-            }
+        }
+
+        if (trobat) {
+
+            return true;
 
         }
 
     }
 
-    return resultado;
+    return false;
 
 }
 
-addCerca('DO', false);
-addCerca('Re', false)
-console.log(cercador(partitures,cerca));
+function cercador(partitures, cerca) {
+    
+    let resultat = [];
+
+    for (let partitura of partitures) {
+
+        if (compararNotas(partitura.notes, cerca)) {
+            resultat.push(partitura.nom);
+        }
+        
+    }
+
+    return resultat;
+}
+
+addCerca('Mi', false);
+addCerca('Sol', false);
+let resultat = cercador(partitures, cerca);
+console.log(resultat);
